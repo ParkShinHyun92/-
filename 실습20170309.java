eab076101883bee8680734a95dc578b029e3aaba
ParkShinHyun92 <umacha@hanyang.ac.kr>
2017-03-09 14:27:04 +0900

    New Program

---
+++ 실습20170309.java
@@ -0,0 +1,24 @@
+package JAVA_실습수업;
+
+import java.util.Scanner;
+
+public class 실습20170309 {
+	void run() {
+		Scanner s = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요.");
+
+		int number = s.nextInt();
+		s.close();
+
+		if(number==1){
+			System.out.print("ParkShinHyun");
+		}else{
+			System.out.print("defalut");
+		}
+		
+		}
+	public static void main(String[] args){
+		실습20170309 t = new 실습20170309();
+	t.run();
+	}
+}
